cf7ea6a0685d60610ac8689cd1f7fa463f59ed4d
aamamun24 <bdmamaun100@gmail.com>
2023-08-21 18:48:16 +0600

    modal completed

--- js/script.js
+++ js/script.js
@@ -10,23 +10,16 @@ function clickProduct(event){
     totalPrice = parseFloat(price) + totalPrice;
     const totalPriceElement = document.getElementById('total-price');
     totalPriceElement.innerText = totalPrice.toFixed(2);
+    
     if(totalPrice > 0){
         const makePurchases = document.getElementById('make-purchase');
         makePurchases.removeAttribute('disabled');
-    };
+    }
     if(totalPrice >= 200){
         const apply = document.getElementById('apply-btn');
         apply.removeAttribute('disabled')
     }
 };
-
-// function makePurchases(){
-//     const totalPrice = document.getElementById('total-price');
-//     if(parseFloat(totalPrice.innerText) === 0){
-//         alert('add to cart')
-//     }
-// };
-
 
 function couponApply(){
     const code = document.getElementById('coupon-code');
@@ -36,55 +29,14 @@ function couponApply(){
         const discount = document.getElementById('discount')
         discount.innerText = percentage.toFixed(2);
         const total = document.getElementById('total');
-        total.innerText = totalPrice - discount.innerText;
-    };
+        const totalAfterDiscount = totalPrice - discount.innerText
+        total.innerText = totalAfterDiscount.toFixed(2);
+    }
+    else{
+        alert('Your coupon code is invalid. Please insert a valid Coupon Code')
+    }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function productName(nameId){
-//     const nameElement = document.getElementById(nameId);
-//     const name = nameElement.innerText;
-//     return name;
-// };
-
-// function productPrice(priceId){
-//     const priceElement = document.getElementById(priceId);
-//     const priceString = priceElement.innerText;
-//     const price = parseFloat(priceString);
-//     return price;
-// }
-
+function goHome(){
+    window.location.href = 'index.html'
+};
