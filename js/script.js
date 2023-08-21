6b30f435e60c58493cac79ac47433f545982b231
aamamun24 <bdmamaun100@gmail.com>
2023-08-21 23:22:18 +0600

    correction

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let totalPrice = 0;
 
-function clickProduct(event){
+function clickProduct(event) {
     const ol = document.getElementById('ol');
     const name = event.childNodes[5].innerText;
     const price = event.childNodes[7].childNodes[0].innerText;
     const li = document.createElement('li');
     li.innerText = name;
     ol.appendChild(li);
-    
+
     totalPrice = parseFloat(price) + totalPrice;
     const totalPriceElement = document.getElementById('total-price');
     totalPriceElement.innerText = totalPrice.toFixed(2);
@@ -15,20 +15,20 @@ function clickProduct(event){
     const total = document.getElementById('total');
     total.innerText = totalPrice.toFixed(2);
 
-    if(totalPrice > 0){
+    if (totalPrice > 0) {
         const makePurchases = document.getElementById('make-purchase');
         makePurchases.removeAttribute('disabled');
     }
-    
-    if(totalPrice >= 200){
+
+    if (totalPrice >= 200) {
         const apply = document.getElementById('apply-btn');
         apply.removeAttribute('disabled')
     }
 };
 
-function couponApply(){
+function couponApply() {
     const code = document.getElementById('coupon-code');
-    if(code.value === 'SELL200'){
+    if (code.value === 'SELL200') {
         const percentage = (20 / 100) * totalPrice;
         const discount = document.getElementById('discount');
         discount.innerText = percentage.toFixed(2);
@@ -37,11 +37,11 @@ function couponApply(){
         const total = document.getElementById('total');
         total.innerText = totalAfterDiscount.toFixed(2);
     }
-    else{
+    else {
         alert('Your coupon code is invalid. Please insert a valid Coupon Code')
     }
 };
 
-function goHome(){
+function goHome() {
     window.location.href = '';
 };
